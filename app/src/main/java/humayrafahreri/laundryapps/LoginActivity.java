d3af655fc353e10c2cbef5ceb221c28a58668b6e
HumayraFahreri <humayrafahreriii@gmail.com>
2024-09-30 23:30:05 +0700

    Praktikum_2

--- app/src/main/java/humayrafahreri/laundryapps/LoginActivity.java
+++ app/src/main/java/humayrafahreri/laundryapps/LoginActivity.java
@@ -1,6 +1,12 @@
 package humayrafahreri.laundryapps;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText edUsername, edPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,32 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //kenalkan widget di dalam method create
+        edUsername = (EditText) findViewById(R.id.edUsername);
+        edPassword = (EditText) findViewById(R.id.edPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        //event handle ketika tombol di klik
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(edUsername.getText().toString())){
+                    Toast.makeText(LoginActivity.this, "Username Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(edPassword.getText().toString())) {
+                    Toast.makeText(LoginActivity.this, "Password Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
+                } else{
+
+                    //intent ketika di klik login akan pindah ke halaman main activity
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra("username", edUsername.getText().toString());
+                    startActivity(intent);
+
+                    //setelah tombol login di klik, maka activity login kita matikan
+                    finish();
+                }
+            }
         });
     }
 }
--- app/src/main/java/humayrafahreri/laundryapps/MainActivity.java
+++ app/src/main/java/humayrafahreri/laundryapps/MainActivity.java
@@ -1,14 +1,23 @@
 package humayrafahreri.laundryapps;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import humayrafahreri.laundryapps.Pelanggan.PelangganActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    String username;
+    CardView cv_laundry, cv_layanan, cv_pelanggan, cv_promo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,47 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        username = getIntent().getStringExtra("username");
+        Toast.makeText(this, " " + username, Toast.LENGTH_SHORT).show();
+
+        cv_laundry=(CardView) findViewById(R.id.cv_laundry);
+        cv_layanan=(CardView) findViewById(R.id.cv_layanan);
+        cv_pelanggan=(CardView) findViewById(R.id.cv_pelanggan);
+        cv_promo=(CardView) findViewById(R.id.cv_promo);
+
+        cv_laundry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "LAUNDRY", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, LaundryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cv_layanan.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, LayananActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cv_pelanggan.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, PelangganActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cv_promo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, PromoActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

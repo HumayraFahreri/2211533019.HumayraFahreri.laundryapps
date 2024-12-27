7e664b25770ebb54c460ca44dcc63e39324caa4d
HumayraFahreri <humayrafahreriii@gmail.com>
2024-12-27 18:47:21 +0700

    ConnectToJson
    ConnectToJson

---
+++ ConnectToJson/app/src/main/java/com/example/connecttojson/Config.java
@@ -0,0 +1,10 @@
+package com.example.connecttojson;
+
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Config {
+    public static String base_url = "http://192.168.218.41/pelanggan.php";
+
+}
